bca0c051797d023153169540ab89dfc7903d715d
ytshen <shenyute@gmail.com>
2012-01-10 23:03:32 +0800

    Install ctrlp plugin with fuzzy file finder

--- install.sh
+++ install.sh
@@ -5,3 +5,9 @@ ln -s vimrc .vim
 cd .vimrc
 git submodule init
 git submodule update
+
+# add new plugin with git submodule
+# example:
+# git submodule add http://github.com/tpope/vim-fugitive.git bundle/fugitive
+# git add .
+# git commit -m "Install Fugitive.vim bundle as a submodule."
