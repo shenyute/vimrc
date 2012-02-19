cfaf54f4b092f76ab0cba0df343050e7c268c97a
ytshen <shenyute@gmail.com>
2012-02-19 21:12:35 +0800

    add quick instruction on README for future usage

--- install.sh
+++ install.sh
@@ -6,8 +6,3 @@ cd .vimrc
 git submodule init
 git submodule update
 
-# add new plugin with git submodule
-# example:
-# git submodule add http://github.com/tpope/vim-fugitive.git bundle/fugitive
-# git add .
-# git commit -m "Install Fugitive.vim bundle as a submodule."
