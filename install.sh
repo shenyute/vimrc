2df0d4fb3bf9fbd50e08a2ab20dedc13f6d93736
ytshen <shenyute@gmail.com>
2012-07-20 10:27:54 +0800

    add tmux config

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ ln -s vimrc/vimrc .vimrc
 ln -s vimrc .vim
 ln -s vimrc/bashrc .bashrc
 ln -s vimrc/screenrc .screenrc
+ln -s vimrc/tmux.conf .tmux.conf
 cd .vim
 git submodule init
 git submodule update
